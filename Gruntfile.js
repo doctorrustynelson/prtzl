3bdd452d100c9f54770e61734fdda2753b534140
Rusty Nelson <doctor.rusty.nelson@gmail.com>
2015-06-14 17:37:32 -0400

    adding grunt as build and test glue

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function (grunt) {
+
+    require( 'time-grunt' )( grunt );
+ 
+    grunt.initConfig({
+		// TODO: configure
+	});
+	
+	// TODO: add build tasks
+	
+	// TODO: add test tasks
+}
