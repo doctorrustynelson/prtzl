1f8016880097b3e871bad29bd7b2123dc9ae55df
Rusty Nelson <doctor.rusty.nelson@gmail.com>
2015-06-14 18:18:28 -0400

    Grunt prints environment.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+var spawn = require( 'child_process' ).spawn;
+var exec = require( 'child_process' ).exec;
+
 module.exports = function (grunt) {
 
     require( 'time-grunt' )( grunt );
@@ -9,4 +12,21 @@ module.exports = function (grunt) {
 	// TODO: add build tasks
 	
 	// TODO: add test tasks
+	
+	grunt.registerTask( 'env', 'Print build environment.', function( ){
+		console.log( 'Environment:' );
+		exec( 'node --version', function( err, stdout, stderr ){
+			console.log( 'node: ' + stdout );
+		} );
+		exec( 'npm --version', function( err, stdout, stderr ){
+			console.log( 'npm: ' + stdout );
+		} );
+		exec( 'ocaml -vnum', function( err, stdout, stderr ){
+			console.log( 'ocaml: ' + stdout );
+		} );
+	} );
+
+	grunt.registerTask( 'default', [
+		'env'
+	] );
 }
