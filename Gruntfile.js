aafb22f9c5b01c1dfd33028b54741f8e03377745
Rusty Nelson <doctor.rusty.nelson@gmail.com>
2015-06-14 19:12:38 -0400

    Added ocamlyacc task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,14 +25,22 @@ module.exports = function (grunt) {
 		ocamllex: {
 			build: {
 				cwd: 'build',
-				src: [ '*.mll' ],
-				dest: 'build'
+				src: [ '*.mll' ]
+			}
+		},
+		
+		ocamlyacc: {
+			build: {
+				cwd: 'build',
+				src: [ '*.mly' ]
 			}
 		}
 	});
 	
 	grunt.registerMultiTask( 'ocamllex', 'Run Ocamllex.', function( ){
 		var done = this.async( );
+		
+		var srcfiles = []
 		
 		this.files.forEach(function(file) {
 			var files = file.src.filter(function (filepath) {
@@ -43,24 +51,73 @@ module.exports = function (grunt) {
 				} else {
 					return true;
 				}
-			}).map( function( filepath ){
-				return path.join( file.cwd, filepath );
+			}).forEach( function( filepath ){
+				srcfiles.push( [ path.join( file.cwd, filepath ) /*, currently building in place so no dest needed */] );
 			});
-			console.log(files);
-			console.log(file.dest);
-			
-			// TODO: Compile
-			
 		});
 		
-		done();
+		var count = srcfiles.length;
+		function completed(){
+			if( ( count -= 1 ) == 0 ){
+				done( true );
+			}
+		}
+		
+		
+		srcfiles.forEach( function( file ){
+			var command = 'ocamllex ' + file; 
+			exec( command, function( err, stdout, stderr ){
+				grunt.log.writeln( command )
+				grunt.log.writeln( '\tstdout: ' + stdout );
+				grunt.log.writeln( '\tstderr: ' + stderr );
+				completed();
+			} );
+		} );		
+	} );
+	
+	grunt.registerMultiTask( 'ocamlyacc', 'Run Ocamlyacc.', function( ){
+		var done = this.async( );
+		
+		var srcfiles = []
+		
+		this.files.forEach(function(file) {
+			var files = file.src.filter(function (filepath) {
+				// Remove nonexistent files (it's up to you to filter or warn here).
+				if (!grunt.file.exists(file.cwd, filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				} else {
+					return true;
+				}
+			}).forEach( function( filepath ){
+				srcfiles.push( [ path.join( file.cwd, filepath ) /*, currently building in place so no dest needed */] );
+			});
+		});
+		
+		var count = srcfiles.length;
+		function completed(){
+			if( ( count -= 1 ) == 0 ){
+				done( true );
+			}
+		}
+		
+		
+		srcfiles.forEach( function( file ){
+			var command = 'ocamlyacc ' + file; 
+			exec( command, function( err, stdout, stderr ){
+				grunt.log.writeln( command )
+				grunt.log.writeln( '\tstdout: ' + stdout );
+				grunt.log.writeln( '\tstderr: ' + stderr );
+				completed();
+			} );
+		} );		
 	} );
 	
 	// TODO: add test tasks
 	
 	grunt.registerTask( 'env', 'Print Build Environment.', function( ){
 		var done = this.async( );
-		var count = 4;
+		var count = 5;
 		
 		function completed(){
 			if( ( count -= 1 ) == 0 ){
@@ -87,6 +144,10 @@ module.exports = function (grunt) {
 			grunt.log.write( '\tocamllex: ' + stdout );
 			completed();
 		} );
+		exec( 'ocamlyacc -vnum', function( err, stdout, stderr ){
+			grunt.log.write( '\tocamlyacc: ' + stdout );
+			completed();
+		} );
 	} );
 
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
@@ -94,7 +155,8 @@ module.exports = function (grunt) {
 
 	grunt.registerTask( 'build', [
 		'copy:build',
-		'ocamllex:build'
+		'ocamllex:build',
+		'ocamlyacc:build'
 	] );
 
 	grunt.registerTask( 'default', [
