d07994cba995914e17f7e53a59335941227fd237
Matt Mallett <mmallett91@gmail.com>
2015-06-21 17:34:15 -0400

    hashmap c library

---
+++ src/clibs/map.h
@@ -0,0 +1,24 @@
+
+
+#define MAP_BUCKETS 8
+
+struct map{
+	int size;
+	struct map_entry** buckets;
+};
+
+struct map_entry{
+	char* key;
+	void* value;
+	struct map_entry* next;
+};
+
+struct map* map_init();
+
+void map_put(struct map*, char*, void* );
+
+void* map_get(struct map*, char*);
+
+void map_del(struct map*, char*);
+
+void map_destroy(struct map*);
